f7cd93334f91ed13670a4e0548d413d78bd45e82
Wor1ock <vanya.yankin9@mail.ru>
2024-05-05 14:54:30 +0400

    feat: добавить метод удаления фильмов

--- src/main/java/com/afisha/filmoteka/controllers/MovieController.java
+++ src/main/java/com/afisha/filmoteka/controllers/MovieController.java
@@ -3,10 +3,12 @@ package com.afisha.filmoteka.controllers;
 import com.afisha.filmoteka.models.Movie;
 import com.afisha.filmoteka.services.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/movies")
 public class MovieController {
 
@@ -18,8 +20,20 @@ public class MovieController {
         return movieService.getAllMovies();
     }
 
+    @GetMapping("/{id}")
+    public Movie getMovieById(@PathVariable Long id) {
+        return movieService.getMovieById(id);
+    }
+
     @PostMapping
     public Movie addMovie(@RequestBody Movie movie) {
         return movieService.addMovie(movie);
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteMovie(@PathVariable Long id) {
+        movieService.deleteMovie(id);
+        return ResponseEntity.ok().build();
+    }
 }
+
--- src/main/java/com/afisha/filmoteka/services/MovieService.java
+++ src/main/java/com/afisha/filmoteka/services/MovieService.java
@@ -17,8 +17,20 @@ public class MovieService {
         return movieRepository.findAll();
     }
 
+    // Получить информацию о конкретном фильме по его идентификатору
+    public Movie getMovieById(Long id) {
+        return movieRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Movie not found with id: " + id));
+    }
+
     // Добавить новый фильм
     public Movie addMovie(Movie movie) {
         return movieRepository.save(movie);
     }
+
+    // Удалить фильм по его идентификатору
+    public void deleteMovie(Long id) {
+        movieRepository.deleteById(id);
+    }
 }
+
